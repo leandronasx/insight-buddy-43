0560740fcb83332f18e79a76616fac97223c0073
Leandro Nascimento <leandronasx18@gmail.com>
2026-05-03 11:05:49 -0300

    correção de instalação PWA, modal para instrução

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,8 +78,11 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    * See https://goo.gl/S9QRab
    */
   workbox.precacheAndRoute([{
+    "url": "suppress-warnings.js",
+    "revision": "d41d8cd98f00b204e9800998ecf8427e"
+  }, {
     "url": "index.html",
-    "revision": "0.ses2b8tol4"
+    "revision": "0.kdigi9uuavk"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
